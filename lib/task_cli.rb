593911bf9f09881095bddd5891046e9f586e5e4a
KarlHarnois <karl@goDynamo.com>
2018-02-23 00:53:06 -0500

    Use our new command matcher in main object
    We clean the main object by using our new command matcher.

--- lib/task_cli.rb
+++ lib/task_cli.rb
@@ -1,27 +1,7 @@
-require 'task_cli/deserializers/task_deserializer'
-require 'task_cli/formatters/task_formatter'
-require 'task_cli/api_client'
+require 'task_cli/command'
 
 class TaskCli
   def self.call(args)
-    new(args).send(:call)
-  end
-
-  private
-
-  def initialize(args)
-    @args = args
-  end
-
-  def call
-    TaskFormatter.new.format_list(tasks)
-  end
-
-  def tasks
-    TaskDeserializer.new(client.fetch_tasks).to_array
-  end
-
-  def client
-    ApiClient.new
+    Command.matching(args).run
   end
 end
