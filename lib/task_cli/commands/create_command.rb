7b49d776ca98e6616821e7fd2328ef76db7489a1
KarlHarnois <karl@goDynamo.com>
2018-02-24 13:45:45 -0500

    Return missing name error for create cmd
    We return early with a meaningful error when the name argument is
    missing when invoking create.

--- lib/task_cli/commands/create_command.rb
+++ lib/task_cli/commands/create_command.rb
@@ -5,8 +5,15 @@ class TaskCli
     kind :create
 
     def run
+      return missing_name_error unless argument
       response = client.create_task(name: argument)
       ['Created task:', "<#{response.id}> #{response.name}"]
+    end
+
+    private
+
+    def missing_name_error
+      'Missing argument <name> in "task_cli create <name>"'
     end
   end
 end
--- spec/commands/create_command_spec.rb
+++ spec/commands/create_command_spec.rb
@@ -17,5 +17,17 @@ RSpec.describe 'create Command' do
         expect(output).to eq ['Created task:', '<1> Configure new HHKB']
       end
     end
+
+    context 'when name argument is absent' do
+      let!(:output) { task_cli 'create' }
+
+      it 'sends no requests' do
+        expect(api_request(:post, '/tasks')).to_not have_been_made
+      end
+
+      it 'returns an error' do
+        expect(output).to eq 'Missing argument <name> in "task_cli create <name>"'
+      end
+    end
   end
 end
