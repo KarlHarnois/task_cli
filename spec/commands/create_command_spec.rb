ef3521f0c35a80aa81028bf6e62dd47b0890dd76
KarlHarnois <karl@goDynamo.com>
2018-02-24 14:19:40 -0500

    Remove top level `describe` in cmd specs
    This is redundant with the initial `describe` statement. We ended up
    describing the command twice.

--- spec/commands/create_command_spec.rb
+++ spec/commands/create_command_spec.rb
@@ -5,29 +5,27 @@ RSpec.describe 'create Command' do
     stub_api(:post, '/tasks').and_return(body: Fixtures.task)
   end
 
-  describe 'create' do
-    context 'when name argument is present' do
-      let!(:output) { task_cli 'create', 'Clean storage' }
+  context 'when name argument is present' do
+    let!(:output) { task_cli 'create', 'Clean storage' }
 
-      it 'sends the correct request' do
-        expect(api_request(:post, '/tasks').with(body: { 'name' => 'Clean storage' })).to have_been_made
-      end
-
-      it 'returns the correct output' do
-        expect(output).to eq ['Created task:', '<1> Configure new HHKB']
-      end
+    it 'sends the correct request' do
+      expect(api_request(:post, '/tasks').with(body: { 'name' => 'Clean storage' })).to have_been_made
     end
 
-    context 'when name argument is absent' do
-      let!(:output) { task_cli 'create' }
+    it 'returns the correct output' do
+      expect(output).to eq ['Created task:', '<1> Configure new HHKB']
+    end
+  end
 
-      it 'sends no requests' do
-        expect(api_request(:post, '/tasks')).to_not have_been_made
-      end
+  context 'when name argument is absent' do
+    let!(:output) { task_cli 'create' }
 
-      it 'returns an error' do
-        expect(output).to eq 'Missing argument <name> in "task_cli create <name>"'
-      end
+    it 'sends no requests' do
+      expect(api_request(:post, '/tasks')).to_not have_been_made
+    end
+
+    it 'returns an error' do
+      expect(output).to eq 'Missing argument <name> in "task_cli create <name>"'
     end
   end
 end
--- spec/commands/get_command_spec.rb
+++ spec/commands/get_command_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe 'get Command' do
-  describe 'get' do
-    context 'when used without options' do
-      let(:output) { task_cli 'get' }
+  context 'when used without options' do
+    let(:output) { task_cli 'get' }
 
-      before do
-        stub_api(:get, '/tasks?state=open').and_return(body: Fixtures.tasks)
-      end
+    before do
+      stub_api(:get, '/tasks?state=open').and_return(body: Fixtures.tasks)
+    end
 
-      it 'returns the open tasks' do
-        expect(output).to eq ['<1> Configure new HHKB',
-                              '<2> Create dotfiles repo',
-                              '<3> Write Cli for task management api']
-      end
+    it 'returns the open tasks' do
+      expect(output).to eq ['<1> Configure new HHKB',
+                            '<2> Create dotfiles repo',
+                            '<3> Write Cli for task management api']
     end
   end
 end
