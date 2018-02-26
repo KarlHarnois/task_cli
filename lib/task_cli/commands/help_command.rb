3fec6f95735e02db146242f82ad56a9db79dc3f1
KarlHarnois <karl@goDynamo.com>
2018-02-25 20:10:51 -0500

    Clean up help summary output
    We create a table feeling by left aligning all of the descriptions and
    putting line breaks between headers.

--- lib/task_cli/commands/help_command.rb
+++ lib/task_cli/commands/help_command.rb
@@ -12,16 +12,17 @@ class TaskCli
 
     def header
       [
-        'Usage: task_cli [COMMAND]',
-        'Type task_cli help [COMMAND] for more details:'
+        "Usage: task_cli [COMMAND]\n\n",
+        "Type task_cli help [COMMAND] for more details:\n\n"
       ]
     end
 
     def commands_summary
-      Command.all
-             .select(&:description)
-             .sort_by(&:name)
-             .map { |cmd| "#{cmd.name}: #{cmd.description}" }
+      Command
+        .all
+        .select(&:description)
+        .sort_by(&:name)
+        .map { |cmd| "#{cmd.name.to_s.ljust(10)} #{cmd.description}" }
     end
   end
 end
--- spec/commands/help_command_spec.rb
+++ spec/commands/help_command_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'help Command' do
   context 'when no argument is passed' do
     it 'returns the correct output' do
-      expect(task_cli).to eq ['Usage: task_cli [COMMAND]',
-                              'Type task_cli help [COMMAND] for more details:',
-                              'complete: mark task as completed',
-                              'get: fetch and display tasks',
-                              'init: create task']
+      expect(task_cli).to eq ["Usage: task_cli [COMMAND]\n\n",
+                              "Type task_cli help [COMMAND] for more details:\n\n",
+                              'complete   mark task as completed',
+                              'get        fetch and display tasks',
+                              'init       create task']
     end
   end
 end
